a83704dbae465b2256d3729d998adf93fc98cec0
Anwesh43 <anweshthecool0@gmail.com>
2019-01-12 12:30:22 +0530

    adding State class

--- triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
+++ triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
@@ -89,4 +89,24 @@ class TriangleSlabStepView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scale.updateScale(dir, slabs, 1)
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
+    }
 }
