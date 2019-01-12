8f4df75892c835cb408b36aaac2aa0d09d9ca68c
Anwesh43 <anweshthecool0@gmail.com>
2019-01-12 12:01:38 +0530

    adding Canvas drawTSSNode canvas extension method

--- triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
+++ triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
@@ -29,3 +29,46 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.clipTrianglePath(size : Float) {
+    val path : Path = Path()
+    path.moveTo(-size/2, size / 2)
+    path.lineTo(size/2, size/2)
+    path.lineTo(0f, -size/2)
+    clipPath(path)
+}
+fun Canvas.drawSlab(x : Float, y : Float, w : Float, h : Float, paint : Paint) {
+    save()
+    translate(x, y)
+    clipTrianglePath(w)
+    paint.style = Paint.Style.STROKE
+    drawRect(-w/2, - h/2, w/2, h/2, paint)
+    paint.style = Paint.Style.FILL
+    drawRect(-w/2, - h/2, w/2, h/2, paint)
+    restore()
+}
+
+fun Canvas.drawTSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sf : Float = 1f - 2 * (i % 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    val x : Float = (w/2 + size/2 + paint.strokeWidth/2)
+    val yGap : Float = h / slabs
+    save()
+    translate(w/2, gap * (i + 1))
+    rotate(180f * sc2)
+    for (j in 0..(slabs - 1)) {
+        val sc : Float = sc1.divideScale(j, slabs)
+        save()
+        drawSlab(x * sc, yGap * (slabs - 1 - j) + yGap / 2, 2 * size, yGap, paint)
+        restore()
+    }
+    restore()
+}
