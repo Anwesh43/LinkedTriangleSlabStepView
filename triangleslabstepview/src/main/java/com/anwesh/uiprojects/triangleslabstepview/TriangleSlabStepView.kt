03524167baf72e94a2ad97e4cc06c3acb608845d
Anwesh43 <anweshthecool0@gmail.com>
2019-01-12 11:31:58 +0530

    adding constants and importing libraries

--- triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
+++ triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.triangleslabstepview
 /**
  * Created by anweshmishra on 12/01/19.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Path
+import android.app.Activity
+
+val nodes : Int = 5
+val slabs : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
