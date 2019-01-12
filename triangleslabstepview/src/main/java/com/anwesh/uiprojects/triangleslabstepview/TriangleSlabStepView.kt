148ce20d2add8fc6c7aaecb58d48fe4f4b23770e
Anwesh43 <anweshthecool0@gmail.com>
2019-01-12 12:03:57 +0530

    adding TriangleSlabStepView class

--- triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
+++ triangleslabstepview/src/main/java/com/anwesh/uiprojects/triangleslabstepview/TriangleSlabStepView.kt
@@ -72,3 +72,21 @@ fun Canvas.drawTSSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TriangleSlabStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
